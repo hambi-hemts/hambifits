d1086838246126d8afc9dd6b62800b1c3f99b7f0
hambi-hemts <hamanna@ethz.ch>
2016-01-05 23:01:17 +0100

    Deprecated class removed

--- __init__.py
+++ __init__.py
@@ -6,8 +6,6 @@ if PyVersion == 2:
     from .py2.helpers import *
     
 elif PyVersion == 3:
-    print("importing from py3 ...")
     from .py3 import classfitfunc
     from .py3.classfitfunc import *
     from .py3 import helpers
-    print("done.")
